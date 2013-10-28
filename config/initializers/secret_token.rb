d32b9f74e106be1737345de1a44283d7a291d071
Wally Glutton <stungeye@gmail.com>
2013-10-28 13:16:31 -0500

    First commit of a default Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NonScaffoldedExample::Application.config.secret_token = 'e487e346a8f23fb9aafd7a3294ba279bac798e75bb3679c944e15f7020814a157a5ee35af9550ae10d0a51399f4a0bd88fea92a457d3f0fd10d4f5224919ee4a'
